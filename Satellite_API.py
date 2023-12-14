98c7933d2903efecf7cbf43dda71d8060bbd6651
mcmonte95 <mike.chiaramonte95@gmail.com>
2023-12-13 20:57:24 -0500

    API created

---
+++ Satellite_API.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Float, String, MetaData, Table
+import pandas as pd
+
+from flask import Flask, jsonify
+
+# Connect to SQLite database SatelliteData.sqlite'
+engine = create_engine('sqlite:///SatelliteData.sqlite')
+
+# Since only one table, can just run a raw query to get the data using read_sql
+sat_df = pd.read_sql("SELECT * FROM satellites", engine)
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/SatLocations<br/>"
+        )
+
+@app.route("/satLocations")
+def sat_locations():
+    return jsonify(sat_df.to_dict(orient='records')) #return the jsonified dataframe
+
+
+#################################################
+# Start app
+#################################################
+if __name__ == '__main__':
+    app.run(debug=False)
--- satallites_db_schema.sql
+++
@@ -1,8 +0,0 @@
--- Create table in Postgres db for satellites data
-CREATE TABLE IF NOT EXISTS satellites (
-    Satellite_Name VARCHAR(100) NOT NULL PRIMARY KEY,
-    Latitude FLOAT NOT NULL,
-    Longitude FLOAT NOT NULL,
-    Distance_from_Earth_Center_km FLOAT NOT NULL,
-    Distance_from_Earth_Surface_km FLOAT NOT NULL
-);
