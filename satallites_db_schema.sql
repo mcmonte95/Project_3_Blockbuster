f9e6cbe7b9748a95d5869df90581c498b989bf30
mcmonte95 <mike.chiaramonte95@gmail.com>
2023-12-13 19:53:23 -0500

    correct table schema and SSC API data

--- satallites_db_schema.sql
+++ satallites_db_schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE satallites (
-    Satallite_ID INT NOT NULL,
-    Satallite_Name VARCHAR (100) NOT NULL,
-    Latitude VARCHAR (100) NOT NULL,
-    Longitude VARCHAR (100) NOT NULL,
-    Distance_from_Earth_Center VARCHAR (100) NOT NULL,
-    Distance_from_Earth_Surface VARCHAR (100) NOT NULL
-	);
+-- Create table in Postgres db for satellites data
+CREATE TABLE IF NOT EXISTS satellites (
+    Satellite_Name VARCHAR(100) NOT NULL PRIMARY KEY,
+    Latitude FLOAT NOT NULL,
+    Longitude FLOAT NOT NULL,
+    Distance_from_Earth_Center_km FLOAT NOT NULL,
+    Distance_from_Earth_Surface_km FLOAT NOT NULL
+);
